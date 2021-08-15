fd4996ca38e71431a2ec7f37beb8187180f24b16
Anshul Kamath <anshulkam@gmail.com>
2021-08-14 22:40:52 -0600

    added automatic test creation for extended gcd

--- scripts/gcd_extended.py
+++ scripts/gcd_extended.py
@@ -104,3 +104,68 @@ def gcd_extended_recursive(a, b, trace=False):
     a, b = max(a, b), min(a, b)
 
     return gcd_extended_helper(a, b, trace)
+
+
+def generate_test_case(ind, num_digits = 1, num_bytes_a=1, num_bytes_b=1, a=None, b=None):
+    if num_digits < num_bytes_a or num_digits < num_bytes_b:
+        raise Exception('Cannot have num_digits < num_bytes')
+    
+    if not a:
+        a = random.randint(0, 1 << 32 * num_bytes_a)
+    if not b:
+        b = random.randint(0, 1 << 32 * num_bytes_b)
+
+    def format_int(num):
+        hex_repr = hex(num)[2:]
+        expected_len = num_digits * 8
+
+        if len(hex_repr) < expected_len:
+            hex_repr = (expected_len - len(hex_repr)) * '0' + hex_repr
+
+        ret_str = ''
+        for i in range(num_digits):
+            ret_str += '0x' + hex_repr[i * 8:(i + 1) * 8] + ', '
+
+        return ret_str[:-2]
+
+    x, y = gcd_extended(a, b)
+    x %= 1 << 32 * num_digits # make sure that x is unsigned
+    y %= 1 << 32 * num_digits # make sure that y is unsigned
+
+    with open(test_file, 'a', newline='') as out:
+        out.write(f'\tconst uint32_t a{ind}[] = {{ {format_int(a)} }};\n')
+        out.write(f'\tconst uint32_t b{ind}[] = {{ {format_int(b)} }};\n')
+        out.write(f'\tconst uint32_t expected_x{ind}[] = {{ {format_int(x)} }};\n')
+        out.write(f'\tconst uint32_t expected_y{ind}[] = {{ {format_int(y)} }};\n')
+        out.write('\n')
+        out.write(f'\tbig_uint_gcd_extended(result_x, result_y, a{ind}, b{ind}, {num_digits});\n')
+        out.write('\n')
+        out.write(f'\texpect(tester, big_uint_equals(expected_x{ind}, result_x, {num_digits}));\n')
+        out.write(f'\texpect(tester, big_uint_equals(expected_y{ind}, result_y, {num_digits}));\n')
+        out.write('\n')
+
+def create_test_cases():
+    # create a new file for the generated test output
+    with open(test_file, 'w+', newline='') as out:
+        out.write('void test_big_uint_gcd_extended() {\n')
+        out.write('\ttesting_logger_t *tester = create_tester();\n')
+        out.write('\tuint32_t result_x[5] = { 0 };\n')
+        out.write('\tuint32_t result_y[5] = { 0 };\n')
+        out.write('\n')
+
+    generate_test_case(1, 1, 1, a=143, b=234)
+    generate_test_case(2, 1, 1, 1)
+    generate_test_case(3, 2, 1, 1)
+    generate_test_case(4, 2, 2, 2)
+    generate_test_case(5, 2, 1, 2)
+    generate_test_case(6, 2, 2, 1)
+    generate_test_case(7, 3, 3, 3)
+    generate_test_case(8, 3, 3, 3)
+    generate_test_case(9, 3, 1, 2)
+    generate_test_case(10, 3, 1, 1)
+
+    with open(test_file, 'a', newline='') as out:
+        out.write('\tlog_tests(tester);\n')
+        out.write('}\n')
+
+create_test_cases()
